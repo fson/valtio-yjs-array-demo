5463cd7809ca9a2c52ca4296d085475aa7b5624a
Ville Immonen <ville.immonen@iki.fi>
2021-11-15 18:15:18 +0200

    Use IndexeddbPersistence

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,81 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import * as Y from "yjs";
+import { IndexeddbPersistence } from "y-indexeddb";
+import { bindProxyAndYArray } from "valtio-yjs";
+import { proxy, useSnapshot } from "valtio";
+import { useState } from "react";
+import { nanoid } from "nanoid/non-secure";
 
-function App() {
-  const [count, setCount] = useState(0)
+type SharedMessage = { id: string; text: string; vote: number }
 
+const ydoc = new Y.Doc();
+
+const provider = new IndexeddbPersistence('messages-v2', ydoc)
+
+provider.on('synced', () => {
+  console.log('content from the database is loaded')
+})
+
+const yarray = ydoc.getArray("messages.v2");
+const messages = proxy([] as SharedMessage[]);
+bindProxyAndYArray(messages, yarray);
+
+const MyMessage = () => {
+  const [message, setMessage] = useState("");
+  const send = () => {
+    if (message) {
+      messages.unshift({ id: nanoid(), text: message, vote: 1 });
+      setMessage("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      Message:{" "}
+      <input value={message} onChange={(e) => setMessage(e.target.value)} />{" "}
+      <button disabled={!message} onClick={send}>
+        Send
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+type MessageProps = { message: SharedMessage } 
+
+const Message = ({ message }: MessageProps) => {
+  const voteUp = () => {
+    const found = messages.find((item) => item.id === message.id);
+    if (found) {
+      found.vote++
+    }
+  };
+  return (
+    <p>
+      [{message.vote}]
+      <button onClick={voteUp}>
+        +1
+      </button>
+      : {message.text}
+    </p>
+  );
+};
+
+const Messages = () => {
+  const snap = useSnapshot(messages);
+  return (
+    <div>
+      {snap.map((message) => (
+        <Message key={message.id} message={message} />
+      ))}
+    </div>
+  );
+};
+
+const App = () => (
+  <div>
+    <h2>My Message</h2>
+    <MyMessage />
+    <h2>Messages</h2>
+    <Messages />
+  </div>
+);
+
+export default App;
